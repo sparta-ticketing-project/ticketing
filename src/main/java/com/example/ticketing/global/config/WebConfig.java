b252ab72ef85aeef0c8f91c969681c135cf5b831
이현수 <mytime0316@naver.com>
2025-03-26 20:28:26 +0900

    feat: 권한 검사 기능(#6)

--- src/main/java/com/example/ticketing/global/config/WebConfig.java
+++ src/main/java/com/example/ticketing/global/config/WebConfig.java
@@ -3,10 +3,12 @@ package com.example.ticketing.global.config;
 import com.example.ticketing.global.auth.AuthUserArgumentResolver;
 import com.example.ticketing.global.auth.JwtFilter;
 import com.example.ticketing.global.auth.JwtUtil;
+import com.example.ticketing.global.auth.UserRoleInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -30,5 +32,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthUserArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserRoleInterceptor())
+                .addPathPatterns("/api/v1/admin/**", "/api/v1/users/**");
     }
 }
