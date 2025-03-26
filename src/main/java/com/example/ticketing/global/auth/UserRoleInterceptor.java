5dee55e4ec26739ea12d97e6968f0f61aa2af4cc
이현수 <mytime0316@naver.com>
2025-03-26 20:28:26 +0900

    feat: 권한 검사 기능(#6)

---
+++ src/main/java/com/example/ticketing/global/auth/UserRoleInterceptor.java
@@ -0,0 +1,34 @@
+package com.example.ticketing.global.auth;
+
+import com.example.ticketing.domain.user.enums.UserRole;
+import com.example.ticketing.global.exception.CustomException;
+import com.example.ticketing.global.exception.ExceptionType;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@RequiredArgsConstructor
+public class UserRoleInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object roleAttr = request.getAttribute("userRole");
+
+        if (roleAttr == null) {
+            throw new CustomException(ExceptionType.REQUIRED_JWT_TOKEN);
+        }
+
+        UserRole userRole = UserRole.of(roleAttr.toString());
+        String requestURI = request.getRequestURI();
+
+        if (requestURI.startsWith("/api/v1/admin") && !userRole.equals(UserRole.ADMIN)) {
+            throw new CustomException(ExceptionType.NO_PERMISSION_ACTION);
+        }
+
+        if (requestURI.startsWith("/api/v1/users") && !userRole.equals(UserRole.USER)) {
+            throw new CustomException(ExceptionType.NO_PERMISSION_ACTION);
+        }
+
+        return true;
+    }
+}
