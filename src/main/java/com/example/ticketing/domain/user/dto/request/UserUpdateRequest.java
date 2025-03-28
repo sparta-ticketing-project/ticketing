1b23267cb2228ac9b68dc26cc4364697560d38ba
karyosome1 <ammonite0880@naver.com>
2025-03-28 14:11:17 +0900

    feat: dto 필드에 유효성 검사 어노테이션 추가 (#4)

--- src/main/java/com/example/ticketing/domain/user/dto/request/UserUpdateRequest.java
+++ src/main/java/com/example/ticketing/domain/user/dto/request/UserUpdateRequest.java
@@ -1,11 +1,16 @@
 package com.example.ticketing.domain.user.dto.request;
 
 import com.example.ticketing.domain.user.enums.Gender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class UserUpdateRequest {
+    @NotBlank
     private String username;
+    @NotNull
     private Gender gender;
+    @NotNull
     private Integer age;
 }
