db89419cdd92c915e31c710d912bc35ee410b81a
Moon YuBin <yubin5744@gmail.com>
2025-03-28 14:09:47 +0900

    feat : 권한 검증 추가

--- src/main/java/com/example/ticketing/domain/user/controller/UserAdminController.java
+++ src/main/java/com/example/ticketing/domain/user/controller/UserAdminController.java
@@ -8,6 +8,8 @@ import com.example.ticketing.domain.user.dto.response.GenderBookingRateResponse;
 import com.example.ticketing.domain.user.dto.response.UpdatedSeatResponse;
 import com.example.ticketing.domain.user.entity.User;
 import com.example.ticketing.domain.user.service.UserAdminService;
+import com.example.ticketing.global.auth.Auth;
+import com.example.ticketing.global.dto.AuthUser;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -28,10 +30,10 @@ public class UserAdminController {
     // 콘서트 생성
     @PostMapping("/api/v1/admin/concerts")
     public ResponseEntity<ConcertResponse> createConcert(
-            //@AuthUser AuthUser authUser
+            @Auth AuthUser authUser,
             @Valid @RequestBody CreateConcertRequest dto ) {
         ConcertResponse concert = userAdminService.createConcert(
-                1L,
+                authUser,
                 dto.getConcertName(),
                 dto.getConcertDate(),
                 dto.getTicketingDate(),
@@ -45,11 +47,11 @@ public class UserAdminController {
     // 콘서트 다건 조회
     @GetMapping("/api/v1/admin/concerts")
     public ResponseEntity<Page<ConcertResponse>> getConcerts(
-            // @AuthUser AuthUser authUser
+            @Auth AuthUser authUser,
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
-        Page<ConcertResponse> concerts = userAdminService.getConcerts(1L, page, size);
+        Page<ConcertResponse> concerts = userAdminService.getConcerts(authUser, page, size);
 
         return ResponseEntity.ok(concerts);
 
@@ -58,20 +60,21 @@ public class UserAdminController {
     // 콘서트 상세 조회
     @GetMapping("/api/v1/admin/concerts/{concertId}")
     public ResponseEntity<ConcertResponse> getConcert(
+            @Auth AuthUser authUser,
             @PathVariable Long concertId
     ) {
-        ConcertResponse concert = userAdminService.getConcert(concertId);
+        ConcertResponse concert = userAdminService.getConcert(authUser, concertId);
         return ResponseEntity.ok(concert);
     }
 
     // 콘서트 정보 수정 (자신이 생성한 콘서트만 수정)
     @PatchMapping("/api/v1/admin/concerts/{concertId}")
     public ResponseEntity<ConcertResponse> updateConcert(
-            // @AuthUser AuthUser authUser
+            @Auth AuthUser authUser,
             @Valid @RequestBody UpdateConcertRequest dto,
             @PathVariable Long concertId) {
         ConcertResponse concert = userAdminService.updateConcert(
-                1L,
+                authUser,
                 concertId,
                 dto.getConcertName(),
                 dto.getConcertDate(),
@@ -87,10 +90,10 @@ public class UserAdminController {
     // 콘서트 삭제 (자신이 생성한 콘서트만 삭제)
     @PostMapping("/api/v1/admin/concerts/{concertId}")
     public ResponseEntity<Map<String, Object>> deleteConcert(
-            // @AuthUSer AuthUser authUser
+            @Auth AuthUser authUser,
             @PathVariable Long concertId
     ) {
-        userAdminService.deleteConcert(1L, concertId);
+        userAdminService.deleteConcert(authUser, concertId);
 
         Map<String, Object> response = new HashMap<>();
         response.put("message", "콘서트가 삭제되었습니다.");
@@ -102,19 +105,21 @@ public class UserAdminController {
     // 좌석 수정
     @PatchMapping("/api/v1/admin/concerts/{concertId}/seats/{seatDetailId}/seat-update")
     public ResponseEntity<UpdatedSeatResponse> updateSeats(
+            @Auth AuthUser authUser,
             @RequestBody UpdateSeatRequest dto,
             @PathVariable Long concertId,
             @PathVariable Long seatDetailId) {
-        UpdatedSeatResponse result = userAdminService.updateSeats(concertId, seatDetailId, dto);
+        UpdatedSeatResponse result = userAdminService.updateSeats(authUser, concertId, seatDetailId, dto);
         return ResponseEntity.ok(result);
     }
 
     // 티켓 예매율 By Gender
     @GetMapping("/api/v1/admin/concerts/{concertId}/booking-rate-by-gender")
     public ResponseEntity<GenderBookingRateResponse> bookingRateByGender(
+            @Auth AuthUser authUser,
             @PathVariable Long concertId
     ) {
-        GenderBookingRateResponse result = userAdminService.bookingRateByGender(concertId);
+        GenderBookingRateResponse result = userAdminService.bookingRateByGender(authUser, concertId);
         return ResponseEntity.ok(result);
     }
 }
